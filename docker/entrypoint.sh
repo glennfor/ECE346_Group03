f62b1dc601d16d047a826302cb3cdcd5ae3c2b65
Shay-7278 <an7278@princeton.edu>
2026-05-03 15:14:23 -0400

    CBF alternative implement

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 # Start virtual framebuffer + VNC + noVNC so RViz/rqt appear in browser at http://localhost:6080
 Xvfb :1 -screen 0 1920x1080x24 &
-x11vnc -display :1 -forever -nopw -quiet &
+sleep 1
+x11vnc -display :1 -forever -nopw &
+sleep 1
 websockify --web /usr/share/novnc 6080 localhost:5900 &
 
 export DISPLAY=:1
